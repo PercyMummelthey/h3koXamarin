57f4409a281ffc528fb3e1dbe5ff7345aa8a5a6c
Percy Mummelthey <Percy.mummelthey@gmail.com>
2017-09-07 11:23:13 +0200

    First

---
+++ XamarinTestApp/XamarinTestApp/XamarinTestAppPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinTestApp
+{
+    public partial class XamarinTestAppPage : ContentPage
+    {
+        public XamarinTestAppPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
